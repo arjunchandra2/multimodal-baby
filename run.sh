8e1c8cb984b62784db83f6d112a1462a06ccee8f
Arjun Chandra <ac25@bu.edu>
2025-02-28 16:44:41 -0500

    set up job request

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
+#!/bin/bash -l
+
+#$ -P ivc-ml            # Specify the SCC project name you want to use
+#$ -l h_rt=11:00:00     # Specify the hard time limit for the job
+#$ -N cvcl-saycamllava  # Give job a name
+#$ -j y                 # Merge the error and output streams into a single file
+#$ -l gpus=1            # One GPU 
+#$ -l gpu_c=8.0         # 8.0 compute capability
+#$ -pe omp 10           # 10 CPU cores
+
+cd /projectnb/ivc-ml/ac25/Baby\ LLaVA/multimodal-baby/
+source .cvcl_env/bin/activate
+
 # full training
 # python training/train.py --batch_size=32 --gpus=1 --num_workers=8 --checkpoint_callback=False --logger=True --pretrained_cnn --max_epochs=5 --multiple_frames --augment_frames
 
@@ -9,7 +22,7 @@
 # python train.py --batch_size=16 --gpus=1 --num_workers=8 --checkpoint_callback=False --logger=False --pretrained_cnn --finetune_cnn --multiple_frames --augment_frames --fast_dev_run --text_encoder="embedding" 
 
 
-
+#Use ./run.sh | tee log.txt if running manually
 #Using args from saycam contrastive.py and any info found in supplementary material (they mostly align)
 python train.py \
     --lambda_mm=1. \
@@ -21,6 +34,7 @@ python train.py \
     --dropout_o=.0 \
     --cnn_dino \
     --pretrained_cnn \
+    --finetune_cnn \
     --multiple_frames \
     --augment_frames \
     --normalize_features \
@@ -42,4 +56,4 @@ python train.py \
     --check_val_every_n_epoch=1 \
     --checkpoint_callback \
     --logger=False \
-    --exp_name="experiment1"
+    --exp_name="experiment_full_2"
